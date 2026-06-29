3a04098a90c3755e2c3b086933c3cb8fe53d2cd8
Knolldeboll <chrissi.ruetz@freenet.de>
2026-06-29 21:32:44 +0200

    opencv.js setup, comments

--- m2m/src/App.tsx
+++ m2m/src/App.tsx
@@ -15,6 +15,12 @@ function App() {
 
     /* Da kommt eigentlich noch "onLoaded = () => {} und openCvPath rein..
      aber das erste funktioniert nicht und wenn man nen Path angibt lädt opencv nicht" */
+
+    /** Es ist so, dass man opencv.js entweder selber baut - das kann man dann mit dem prop "openCvPath" hier angeben - 
+     * oder man gibt nix an, dann zieht der sich die lib von https://docs.opencv.org/3.4.20/opencv.js oder so runter.
+     * Dauert jetzt auch nicht ewig, von dem her kein Problem.
+     * ggf. sollte man das in ner echten Anwendung cachen, damit das nicht jedes mal geladen werden muss.
+     */
     <OpenCvProvider  >
       <OpenCVComponent></OpenCVComponent>
     </OpenCvProvider>
